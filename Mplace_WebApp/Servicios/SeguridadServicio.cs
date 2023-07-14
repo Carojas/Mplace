3bc31db4185dbbd015d36b68ed6895b23d5f6bfc
cristian0003carb <cristian.0003.carb@gmail.com>
2023-07-13 21:04:51 -0500

    Ajuste

--- Mplace_WebApp/Servicios/SeguridadServicio.cs
+++ Mplace_WebApp/Servicios/SeguridadServicio.cs
@@ -1,6 +1,15 @@
-﻿namespace Mplace_WebApp.Servicios
+﻿using Mplace_WebApp.Areas.Identity.Data;
+
+namespace Mplace_WebApp.Servicios
 {
     public class SeguridadServicio
     {
+        public void RegistrarUsuario(Usuario usuario) 
+        {
+
+        }
+        public void Login()
+        {
+        }
     }
 }
